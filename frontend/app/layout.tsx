d790861c2fffeffa47c58177af2f93d995ee4acb
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-12-29 01:04:16 +0100

    ...

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,10 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/Navbar";
-import { ToastProvider } from "./components/ToastProvider"; // Import ToastProvider
+import { ToastProvider } from "./components/ToastProvider"; 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,7 +25,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+     
       >
         <ToastProvider>
           <Navbar />
